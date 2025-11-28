388606913e09cf46053c8c4aa7d0b97747db5b45
XNS-ivy <ayramusic.nightcore@gmail.com>
2025-11-28 10:56:58 +0700

    base commit

---
+++ app/index.ts
@@ -0,0 +1,33 @@
+import { Elysia, t } from 'elysia'
+const port = 3000
+const app = new Elysia()
+
+app.get('/', () => "Halo, ini Elysia")
+
+app.get('/user/:name', ({ params }) => {
+    console.log(params)
+    return `Nama kamu: ${params.name}`
+})
+
+app.post('/login', ({ body }: any) => {
+    return {
+        message: `Login sebagai ${body.username}`
+    }
+})
+
+app.post('/register', ({ body }: any) => {
+    return {
+        status: "success",
+        username: body.username
+    }
+})
+
+app.post('/math/add', ({ body }: any) => {
+    return {
+        hasil: body.x + body.y
+    }
+})
+
+app.listen(port)
+
+console.log(`🟢 Server running at http://localhost:${port}`)
