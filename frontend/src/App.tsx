e86811858c663b1c274843f4927fc6a18954d119
zetsuna <felsa.jr1205@gmail.com>
2025-12-24 08:56:35 +0700

    main tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,42 @@
-import Header from "./components/Header/Header";
-import Carousel from "./components/Carousel/Carousel";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { CartProvider } from './context/CartContext'
+import { Header } from './components/common/Header'
+import { Footer } from './components/common/Footer'
+import { ErrorBoundary } from './components/common/ErrorBoundary'
+import { Dashboard } from './pages/Dashboard'
+import { Products } from './pages/Products'
+import { ProductDetail } from './pages/ProductDetail'
+import { Cart } from './pages/Cart'
+import { Checkout } from './pages/Checkout'
+import { OrderConfirmation } from './pages/OrderConfirmation'
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <>
-      <Header />
-      <div className="max-w-[1208px] mt-[130px] min-h-screen mx-auto">
-        <Carousel />
-      </div>
-    </>
-  );
+    <BrowserRouter>
+      <ErrorBoundary>
+        <AuthProvider>
+          <CartProvider>
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex-grow bg-gray-50">
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/products" element={<Products />} />
+                  <Route path="/products/:id" element={<ProductDetail />} />
+                  <Route path="/cart" element={<Cart />} />
+                  <Route path="/checkout" element={<Checkout />} />
+                  <Route path="/order-confirmation/: orderId" element={<OrderConfirmation />} />
+                </Routes>
+              </main>
+              <Footer />
+            </div>
+          </CartProvider>
+        </AuthProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  )
 }
+
+export default App
